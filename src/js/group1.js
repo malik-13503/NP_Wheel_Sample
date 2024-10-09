53953e8e9e044c089fa1a2b5f71ae0198ac3eb49
malik-13503 <95710499+malik-13503@users.noreply.github.com>
2024-10-09 16:13:17 +0500

    Add files via upload

---
+++ src/js/group1.js
@@ -0,0 +1,43 @@
+questions = [
+  {
+    q: 'Which of these lubricants is the synthetic engine oil from the Ultra range of products?',
+    a: 'Ultra Tec 10W-30',
+    choices: ['Ultra Supermatic', 'Ultra Tec 10W-30', 'Ultra Premium 10W-30', 'Ultra Diesel Plus 50']
+  },
+  // {
+  //   q: 'Which Ultra lubricant can be used in both diesel and gasoline engines?',
+  //   a: 'Ultra Duty 15W-40',
+  //   choices: ['Ultra DTV 20W-50', 'Ultra Duty 15W-40', 'Ultra Superstream', 'Ultra Premium 20W-50']
+  // },
+  {
+    q: 'Is Ultra Tec 0W-20 (Hybrid) used for hybrid gasoline engines?',
+    a: 'Yes',
+    choices: ['Yes', 'No']
+  },
+  {
+    q: 'Which is NOT a feature of NP Gas composite cylinders?',
+    a: 'Corrodes easily',
+    choices: ['Lightweight', 'Safe', 'Translucent', 'Corrodes easily']
+  },
+  // {
+  //   q: 'Which statement below is FALSE?',
+  //   a: 'Cylinders can be transported lying down in your vehicle',
+  //   choices: [
+  //     'Cylinders can be transported lying down in your vehicle',
+  //     'Cylinders must be stored in a well-ventilated location away from sources of ignition',
+  //     'If a leak is suspected, move the cylinder outdoors and cover it with a damp cloth',
+  //     'Cylinders should not be kept unused for more than 6 months'
+  //   ]
+  // },
+    {
+    q: 'It is an unsafe practice to transport filled cylinders in your vehicle lying down.',
+    a: 'Yes',
+    choices: [
+      'Yes',
+      'No'
+    ]
+  }
+  
+];
+
+
